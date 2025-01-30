9eab91b7278764344099208792fd29002226750a
niloyahmedrasel <niloyahmedrasel24@gmail.com>
2025-01-30 12:38:50 +0600

    fixed bugs

--- src/model/ticket.ts
+++ src/model/ticket.ts
@@ -6,8 +6,8 @@ const ticketSchema: Schema<ITicket> = new Schema(
     title: { type: String, required: true },
     projectId: { type: Schema.Types.ObjectId, ref: "Project", required: true },
     description: { type: String, required: true },
-    assignedTo: { type: Schema.Types.ObjectId,enum:["Minor", "Major", "Critical","Blocker"], ref: "User", required: true },
-    priority: { type: String, required: true },
+    assignedTo: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    priority: { type: String,enum:["Minor", "Major", "Critical","Blocker"], required: true },
   },
   {
     timestamps: true,
